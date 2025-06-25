eefc6af6d580ef9fa7f092f5bc5bfe4a809a1d0c
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-25 13:04:59 +0500

    created

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,82 @@
-import Image from 'next/image'
-import React from 'react'
 
+import React from "react";
+import { FaFacebook, FaInstagramSquare, FaLinkedin, FaWhatsapp } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 const Footer = () => {
   return (
-    <div className='w-full h-auto flex justify-center items-center py-20'>
-        <div className='w-[90%] h-auto flex flex-col gap-3 justify-center items-center'>
-                <div className="heading w-full h-auto">
-                        <h2 className='text-primary text-8xl'>Let’s Brew a Connection</h2>
-                </div>
-                <div className='flex gap-6 justify-between h-auto w-full items-start'>
-                        <div className="w-[60%] flex flex-col gap-3">
-                                <p className='font-sans font-normal text-base'>Got a question, a suggestion, or just want to say hello? We’d love to hear from you!</p>
-                                
-                        </div>
-                        <div className="w-[40%]">
-                                <Image src='/images/footer-image.png' alt='iamge' width={550} height={550} />
-                        </div>
-                </div>
-                <div className="copyright w-full text-center py-4 flex justify-center items-center">
-                        <p className='md:text-base text-sm font-sans'>© 2021 all copyright reserved. All Rights Reserved.</p>
-                </div>
+    <div className="w-full h-auto flex justify-center items-center pb-10 pt-20">
+      <div className="w-[90%] h-auto flex flex-col gap-3 justify-center items-center">
+        <div className="heading w-full h-auto">
+          <h2 className="text-primary text-8xl">Let’s Brew a Connection</h2>
         </div>
+        <div className="flex gap-6 justify-between h-auto w-full items-start">
+          <div className="w-[60%] flex flex-col gap-6">
+            <p className="font-sans text-primary font-normal text-base">
+              Got a question, a suggestion, or just want to say hello? We’d love
+              to hear from you!
+            </p>
+            <div className="info flex flex-col py-3 gap-4 ">
+              <div className="flex gap-1">
+                <span className="text-primary font-sans font-bold">
+                  📧 Email:
+                </span>
+                <span className="font-sans font-normal text-primary">
+                  info@[yourbrandname].com
+                </span>
+              </div>
+              <div className="flex gap-1">
+                <span className="text-primary font-sans font-bold">
+                  📞 Phone:
+                </span>
+                <span className="font-sans font-normal text-primary">
+                  +1-234-567-890
+                </span>
+              </div>
+              <div className="flex gap-1">
+                <span className="text-primary font-sans font-bold">
+                  📍 Address:
+                </span>
+                <span className="font-sans font-normal text-primary">
+                  123 Chai Street, Flavor Town, USA
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-col gap-3">
+              <h2 className="text-primary text-5xl">Stay in Touch</h2>
+              <p className="text-sm max-w-md text-primary font-sans">
+                Follow us on social media and subscribe to our newsletter for
+                updates, offers, and chai inspiration.
+              </p>
+               <div className="social-links text-secondary text-2xl flex gap-3">
+              <FaFacebook />
+              <FaXTwitter />
+              <FaLinkedin />
+              <FaInstagramSquare />
+              <FaWhatsapp />
+            </div>
+            </div>
+          </div>
+          <div
+            className="w-[40%] h-[450px] bg-center bg-cover"
+            style={{ backgroundImage: `url('/images/footer-image.png')` }}
+          >
+            {/* <Image
+              src="/images/footer-image.png"
+              alt="iamge"
+              width={550}
+              height={550}
+              className="h-auto w-auto"
+            /> */}
+          </div>
+        </div>
+        <div className="copyright w-full text-center pt-2 flex justify-center items-center">
+          <p className="md:text-base text-sm font-sans">
+            © 2021 all copyright reserved. All Rights Reserved.
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
